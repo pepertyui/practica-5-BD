4ffdf366e78c33f477f2f3e6e52262070c7ae4c2
pepertyui <pepe_davila@live.com.mx>
2014-11-20 12:45:25 -0600

    programa terminado

--- practica5/practica5/MySQL.cs
+++ practica5/practica5/MySQL.cs
@@ -7,16 +7,35 @@
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
-
-namespace practica5
+using MySql.Data.MySqlClient;
+namespace practica55
 {
 	/// <summary>
 	/// Description of MySQL.
 	/// </summary>
 	public class MySQL
 	{
+		protected MySqlConnection myConnection;
 		public MySQL()
 		{
+	
 		}
+		protected void abrirConexion(){
+			string connectionString =
+          		"Server=localhost;" +
+	          	"Database=agenda;" +
+	          	"User ID=root;" +
+	          	"Password=pepe;" +
+	          	"Pooling=false;";
+	       this.myConnection = new MySqlConnection(connectionString);
+	       this.myConnection.Open();
+		}
+		
+		protected void cerrarConexion(){
+			this.myConnection.Close(); 
+			this.myConnection = null;
+		}				
+		
+		
 	}
 }
